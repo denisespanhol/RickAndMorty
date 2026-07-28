6c4b640c91f346c45effac3b215661ce565a9e60
Denis Guilherme Ferreira Espanhol <denisf@ciandt.com>
2026-07-28 13:31:41 -0300

    feat: Create HomeView and adding it in RootView

---
+++ Rick-and-Morty-App/Rick-and-Morty-App/Home/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  HomeView.swift
+//  Rick-and-Morty-App
+//
+//  Created by Denis Guilherme Ferreira Espanhol on 28/07/26.
+//
+
+import SwiftUI
+import DSM
+
+struct HomeView: View {
+    
+    @EnvironmentObject private var appState: AppState
+    
+    var body: some View {
+        TabView {
+            /*CharacterListView()
+                .tabItem { Label("Characters", systemImage: "person.2.fill") }
+             LocationListView()
+                 .tabItem { Label("Locations", systemImage: "map.fill") }
+             
+             EpisodeListView()
+                 .tabItem { Label("Episodes", systemImage: "tv.fill") }  */
+        }
+    }
+}
--- Rick-and-Morty-App/Rick-and-Morty-App/RickMortyApp/RootView.swift
+++ Rick-and-Morty-App/Rick-and-Morty-App/RickMortyApp/RootView.swift
@@ -14,7 +14,7 @@ struct RootView: View {
     
     var body: some View {
         if appState.isLoggedIn {
-            //HomeView()
+            HomeView()
         } else {
             LoginModule.makeLoginView(delegate: LoginDelegate(appState: appState))
         }
