f367b121b2250e32e038af7b0429413607898cb3
Denis Guilherme Ferreira Espanhol <denisf@ciandt.com>
2026-07-29 09:24:38 -0300

    Implementing CharacterDetailViewModel to get API infos

--- Rick-and-Morty-App/Rick-and-Morty-App/Characters/CharacterDetailViewModel.swift
+++ Rick-and-Morty-App/Rick-and-Morty-App/Characters/CharacterDetailViewModel.swift
@@ -5,3 +5,36 @@
 //  Created by Denis Guilherme Ferreira Espanhol on 28/07/26.
 //
 
+import Foundation
+import Combine
+import NetworkKit
+
+@MainActor
+final class CharacterDetailViewModel: ObservableObject {
+    
+    let character: Character
+    
+    init(character: Character) {
+        self.character = character
+    }
+    
+    var statusAndSpecies: String {
+        "\(character.status) · \(character.species)"
+    }
+    
+    var originName: String {
+        character.origin.name
+    }
+    
+    var locationName: String {
+        character.location.name
+    }
+    
+    var episodeCount: String {
+        "\(character.episode.count) episodes"
+    }
+    
+    var imageURL: URL? {
+        URL(string: character.image)
+    }
+}
