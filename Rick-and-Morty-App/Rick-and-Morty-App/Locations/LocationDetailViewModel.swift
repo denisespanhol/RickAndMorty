129e7ac9c8e587295b92b0e924b9da9fde89091e
Denis Guilherme Ferreira Espanhol <denisf@ciandt.com>
2026-07-30 09:05:47 -0300

    feat: create LocationDetailViewModel

--- Rick-and-Morty-App/Rick-and-Morty-App/Locations/LocationDetailViewModel.swift
+++ Rick-and-Morty-App/Rick-and-Morty-App/Locations/LocationDetailViewModel.swift
@@ -5,3 +5,24 @@
 //  Created by Denis Guilherme Ferreira Espanhol on 29/07/26.
 //
 
+import Foundation
+import Combine
+import NetworkKit
+
+@MainActor
+final class LocationDetailViewModel: ObservableObject {
+    
+    let location: Location
+    
+    init(location: Location) {
+        self.location = location
+    }
+    
+    var typeAndDimension: String {
+        "\(location.type) · \(location.dimension)"
+    }
+    
+    var residentsCount: String {
+        "\(location.residents.count) residents"
+    }
+}
