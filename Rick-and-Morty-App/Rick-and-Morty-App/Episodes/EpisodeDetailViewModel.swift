5308ccd1e452be831b3382330b9d6fb6a524d053
Denis Guilherme Ferreira Espanhol <denisf@ciandt.com>
2026-07-30 10:57:24 -0300

    feat: implementing EpisodeDetailsViewModel to receive episode details from NetworkKit

--- Rick-and-Morty-App/Rick-and-Morty-App/Episodes/EpisodeDetailViewModel.swift
+++ Rick-and-Morty-App/Rick-and-Morty-App/Episodes/EpisodeDetailViewModel.swift
@@ -5,3 +5,28 @@
 //  Created by Denis Guilherme Ferreira Espanhol on 29/07/26.
 //
 
+import Foundation
+import Combine
+import NetworkKit
+
+@MainActor
+final class EpisodeDetailViewModel: ObservableObject {
+    
+    let episode: Episode
+    
+    init(episode: Episode) {
+        self.episode = episode
+    }
+    
+    var episodeCode: String {
+        episode.episode
+    }
+    
+    var airDate: String {
+        episode.airDate
+    }
+    
+    var charactersCount: String {
+        "\(episode.characters.count) characters"
+    }
+}
